1fae30c3e861a2d60eae80b73c71aa7b77ffdb37
sethgonzales <sethgonzales157@gmail.com>
2023-10-03 16:04:21 -0700

    final project

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -19,5 +19,28 @@ namespace ToDoList.Controllers
       List<Item> model = _db.Items.ToList(); //use the database model instance to 
       return View(model);
     }
+
+    public ActionResult Create() //take us to the form page
+    {
+      return View();
+    }
+
+    [HttpPost] 
+    public ActionResult Create(Item item) //take in form data with post request, assign to variables, and redirect to index
+    {
+      _db.Items.Add(item);  //add new item to the items list using db abilities
+      _db.SaveChanges(); // build in method to save changes in our directory
+      return RedirectToAction("Index"); //take us to the index
+    }
+
+    public ActionResult Details(int id)
+    {
+      Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id); //in our list of items (_db.Items) give me the first (FirstOrDefault) item (item) whose (=>) Id (item.ItemId) matches the id (id) we passed in
+      return View(thisItem);
+    }
+
   }
 }
+
+
+
